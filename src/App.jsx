9776f074ba4d9d2c06d747c55c6f8b59578d4a36
Viraj2112 <virajrnagarkar21@gmail.com>
2025-01-17 20:11:57 +0530

    Create React + Vite + TailwindCss Project Template

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='container mx-auto my-10'>
+        <h1 className='font-playwrite-in  text-6xl font-bold text-green-500'>Hello world</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
